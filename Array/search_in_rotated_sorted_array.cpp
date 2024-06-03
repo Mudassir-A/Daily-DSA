54ee3157e553f191c8df64d74945341c874f8eb8
Mohd Mudassir Ansari <mudassirimranansari@gmail.com>
2024-06-04 02:11:05 +0530

    6. Search in Rotated Sorted Array

---
+++ Array/search_in_rotated_sorted_array.cpp
@@ -0,0 +1,50 @@
+// #include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(int arr[], int tar, int si, int ei)
+{
+    int mid = si + (ei - si) / 2;
+
+    // case: FOUND
+    if (arr[mid] == tar)
+        return mid;
+
+    // min on L1
+    if (arr[si] <= arr[mid])
+    {
+        // case a: left
+        if (arr[si] <= tar && tar <= arr[mid])
+        {
+            return search(arr, tar, si, mid - 1);
+        }
+        // case b: right
+        else
+        {
+            return search(arr, tar, mid + 1, ei);
+        }
+    }
+
+    else
+    {
+        // case a: right
+        if (arr[mid] <= tar && tar <= arr[ei])
+        {
+            return search(arr, tar, mid + 1, ei);
+        }
+        // case d: left
+        else
+        {
+            return search(arr, tar, si, mid - 1);
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {4, 5, 6, 7, 0, 1, 2};
+    int tar = 6;
+    int size = sizeof(arr) / sizeof(arr[0]);
+    cout << search(arr, tar, 0, size);
+    return 0;
+}
