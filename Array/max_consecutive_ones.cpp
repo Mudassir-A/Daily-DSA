605dabbcf72d77ab80b774f85ca5d6f653238e64
Mohd Mudassir Ansari <mudassirimranansari@gmail.com>
2024-07-05 11:58:11 +0530

    37. Max consecutive ones

---
+++ Array/max_consecutive_ones.cpp
@@ -0,0 +1,38 @@
+// #include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMaxConsecutiveOnes(vector<int> &nums)
+{
+    int cnt = 0;
+    int maxi = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == 1)
+        {
+            cnt++;
+        }
+        else
+        {
+            cnt = 0;
+        }
+
+        maxi = max(maxi, cnt);
+    }
+    return maxi;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << findMaxConsecutiveOnes(arr);
+    return 0;
+}
